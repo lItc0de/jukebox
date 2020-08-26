777347c85c2c2b33f19e02e0255a9695557ae315
litc0de <litc0de@posteo.org>
2020-08-26 21:36:37 +0200

    show more information for playlists

--- frontend/src/Home.vue
+++ frontend/src/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div>
+  <div class="home-page">
     <ApolloQuery
       :query="require('./graphql/Playlists.gql')"
       :variables="{ first: 50, cursor: '' }"
     >
       <template slot-scope="{ result: { loading, error, data } }">
         <div v-if="loading">Loading</div>
-        <c-playlist
-          v-else-if="data"
-          v-for="edge in data.playlists.edges"
-          :playlist="edge.node"
-          :key="edge.node.id"
-        />
+        <ul v-else-if="data" class="playlists">
+          <li class="playlists-header">
+            <span />
+            <span class="name">Name</span>
+            <span>Owner</span>
+            <span>Tracks</span>
+          </li>
+          <c-playlist
+            v-for="edge in data.playlists.edges"
+            :playlist="edge.node"
+            :key="edge.node.id"
+          />
+        </ul>
         <div v-else-if="error">{{ error.message }}</div>
       </template>
     </ApolloQuery>
@@ -29,3 +36,24 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+.home-page
+  padding 1rem
+  max-width 700px
+
+.playlists
+  margin 0
+  padding 0
+
+  > .playlists-header
+    display flex
+    flex-direction row
+    justify-content space-between
+
+    > span
+      flex 1
+
+    > .name
+      flex 3
+</style>
--- frontend/src/components/Playlist.vue
+++ frontend/src/components/Playlist.vue
@@ -1,7 +1,12 @@
 <template>
-  <ul class="playlist">
-    <h1>{{ playlist.name }}</h1>
-  </ul>
+  <li class="playlist">
+    <span class="image">
+      <img :src="playlist.images[0].url" :alt="playlist.name" />
+    </span>
+    <span class="name">{{ playlist.name }}</span>
+    <span>{{ playlist.owner.displayName }}</span>
+    <span>{{ playlist.tracks.total }}</span>
+  </li>
 </template>
 
 <script>
@@ -21,4 +26,22 @@ export default {
 .playlist
   margin 0
   padding 0
+  display flex
+  flex-direction row
+  justify-content space-between
+  align-items center
+  border-top 1px solid var(--borderColor)
+
+  > span
+    flex 1
+    padding 0 .5rem
+
+  > .name
+    flex 3
+
+  > .image
+    line-height 0
+    padding 0
+    > img
+      height 4rem
 </style>
